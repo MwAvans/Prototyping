c11d0387c9a781a93811dca43e2d8df1d1596251
MwAvans <124674179+MwAvans@users.noreply.github.com>
2023-05-10 21:58:58 +0200

    Update prototyping.py

--- prototyping.py
+++ prototyping.py
@@ -8,6 +8,24 @@ st.title("Data scientists salaries")
 
 upload_file = st.file_uploader('Upload a file containing earthquake data')
 
+
+def add_parameter_ui(clf_name):
+    params = dict()
+    if clf_name == 'SVM':
+        C = st.sidebar.slider('C', 0.01, 10.0)
+        params['C'] = C
+    elif clf_name == 'KNN':
+        K = st.sidebar.slider('K', 1, 15)
+        params['K'] = K
+    else:
+        max_depth = st.sidebar.slider('max_depth', 2, 15)
+        params['max_depth'] = max_depth
+        n_estimators = st.sidebar.slider('n_estimators', 1, 100)
+        params['n_estimators'] = n_estimators
+    return params
+
+params = add_parameter_ui(classifier_name)
+
 # Check to see if a file has been uploaded
 if upload_file is not None:
     # If it has then do the following:
