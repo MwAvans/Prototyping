6b5b0df3c16fa4bbc03d4c985e6f9e546289be71
MwAvans <124674179+MwAvans@users.noreply.github.com>
2023-05-10 20:39:55 +0200

    Update prototyping.py

--- prototyping.py
+++ prototyping.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LogisticRegression
 
 st.title("Data scientists salaries")
 
