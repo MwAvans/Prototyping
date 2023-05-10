5dcc00194c9dcd574bd6163b1e623d1c3421de65
MwAvans <124674179+MwAvans@users.noreply.github.com>
2023-05-10 20:41:45 +0200

    Update prototyping.py

--- prototyping.py
+++ prototyping.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import LogisticRegression
-
+import matplotlib.pyplot as plt
 st.title("Data scientists salaries")
 
 upload_file = st.file_uploader('Upload a file containing earthquake data')
