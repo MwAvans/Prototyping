2a68e24f2396b9d3174a5349395c5a237f01b789
MwAvans <124674179+MwAvans@users.noreply.github.com>
2023-05-10 20:33:39 +0200

    Update prototyping.py

--- prototyping.py
+++ prototyping.py
@@ -1,3 +1,30 @@
 import streamlit as st
 
-st.title("Data scientists salaroes")
+st.title("Data scientists salaries")
+
+upload_file = st.file_uploader('Upload a file containing earthquake data')
+
+# Check to see if a file has been uploaded
+if upload_file is not None:
+    # If it has then do the following:
+
+    # Read the file to a dataframe using pandas
+    df = pd.read_csv(upload_file)
+
+    # Create a section for the dataframe statistics
+    st.header('Statistics of Dataframe')
+    st.write(df.describe())
+
+    # Create a section for the dataframe header
+    st.header('Header of Dataframe')
+    st.write(df.head())
+
+    # Create a section for matplotlib figure
+    st.header('Plot of Data')
+    
+    fig, ax = plt.subplots(1,1)
+    ax.scatter(x=df['Depth'], y=df['Magnitude'])
+    ax.set_xlabel('Depth')
+    ax.set_ylabel('Magnitude')
+    
+    st.pyplot(fig)
