aa89f16c56cb95f8a0a3ba92375d799df90cbe65
MwAvans <124674179+MwAvans@users.noreply.github.com>
2023-05-11 22:25:08 +0200

    Update prototyping.py

--- prototyping.py
+++ prototyping.py
@@ -39,7 +39,7 @@ if upload_file is not None:
     from joblib import dump, load
 
     #Load your trained models and csv file
-    model = load(str(str(os.getcwd())+'/Decisiontree.good.joblib'))
+    model = open(str(str(os.getcwd())+'/Decisiontree.good.joblib'))
     model = load(str(str('/Decisiontree.good.joblib'))) 
     df= salaries
 
